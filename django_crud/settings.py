f70ecb75a76968d241d3fdc6bb0e5cb7ea0f7112
Fuffo <ayalafabian576@gmail.com>
2023-08-10 00:17:39 -0400

    Actualizada database momentaneamente

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'django_crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
